b2ea59b910caa9e18ae431e47afd67707b4ef49a
Mateus Maia <maiamateusotavio@gmail.com>
2022-05-07 08:44:45 -0300

    first commit

---
+++ src/main/java/com/mateusmaia/cadastrosimples/CadastrosimplesApplication.java
@@ -0,0 +1,13 @@
+package com.mateusmaia.cadastrosimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosimplesApplication.class, args);
+	}
+
+}
