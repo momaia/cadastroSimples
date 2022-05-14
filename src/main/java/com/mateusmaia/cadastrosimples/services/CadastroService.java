b485db0555b2d178db312a9624993cc246ab2431
Mateus Maia <maiamateusotavio@gmail.com>
2022-05-14 11:32:20 -0300

    Inclusao do metodo DELETE no servico de cadastro

--- src/main/java/com/mateusmaia/cadastrosimples/services/CadastroService.java
+++ src/main/java/com/mateusmaia/cadastrosimples/services/CadastroService.java
@@ -3,10 +3,12 @@ package com.mateusmaia.cadastrosimples.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mateusmaia.cadastrosimples.domain.CadastroDomain;
 import com.mateusmaia.cadastrosimples.repositories.CadastroRepository;
+import com.mateusmaia.cadastrosimples.services.exceptions.DataIntegrityException;
 
 @Service
 public class CadastroService {
@@ -27,5 +29,15 @@ public class CadastroService {
 	public CadastroDomain update(CadastroDomain obj){
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cadastro que possui endereço");
+		}
 	}
 }
